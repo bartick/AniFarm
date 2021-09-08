04cacca413072ed1b5ebd7571ee9311e87cf4a04
bartick <bartickmaiti1998@gmail.com>
2021-09-08 15:03:20 +0530

    A new style

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const anifarm = require('./../models/anifarm')
+const wait = require('util').promisify(setTimeout);
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Shows Bot Latency 🏓'),
     async execute(interaction) {
-        await interaction.reply(`🏓 WS: ${interaction.client.ws.ping}`)
-        const d = await anifarm.findById(interaction.user.id).exec();
-        console.log(d);
+        await interaction.reply('🏓 Pong');
+        await wait(1500);
+        await interaction.editReply(`🏓 WS: ${"`"}${interaction.client.ws.ping}${"`"}`);
     },
 };
