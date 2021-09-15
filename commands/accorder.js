9e77e17396fe203954f21a378585284fce4d317e
bartick <bartickmaiti1998@gmail.com>
2021-09-15 15:31:27 +0530

    Transfer

--- commands/accorder.js
+++ commands/accorder.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const orders = require('./../models/orders');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,6 +12,32 @@ module.exports = {
                 .setRequired(true)
             ),
     async execute(interaction) {
-        //TODO: complete accept order
+        orderid = interaction.options.getInteger('orderid');
+        const gameOrder = await orders.findOne({
+            orderid: orderid,
+            guildid: interaction.guild.id,
+            farmerid: "0"
+        })
+        if (gameOrder===null) {
+            await interaction.reply({
+                ephemeral: true,
+                embeds: [
+                    new MessageEmbed()
+                        .setColor('RED')
+                        .setThumbnail(interaction.client.user.displayAvatarURL({dynamic: true, size: 1024}))
+                        .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({dynamic: true, size: 1024}))
+                        .setTimestamp()
+                        .setTitle('⛔️ Error')
+                        .setDescription('Cannot find the order.\nEither the order is accepted or there is no order with this id.')
+                ]
+            });
+            return;
+        }
+        //Check if it is a farmer
+        console.log(gameOrder);
+        await interaction.reply({
+            ephemeral: true,
+            content: 'hello'
+        })
     }
 };
