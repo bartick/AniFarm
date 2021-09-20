03d05a8c1e2d1fbc8418aff9fb6aae93637fb94c
bartick <bartickmaiti1998@gmail.com>
2021-09-20 11:19:27 +0530

    Removed un-necessary comments

--- utils/relateDate.js
+++ utils/relateDate.js
@@ -8,16 +8,7 @@ TimeAgo.addDefaultLocale(en)
 // Create formatter (English).
 const timeAgo = new TimeAgo('en-US')
 
-// timeAgo.format(new Date())
-// // "just now"
-
 // timeAgo.format(Date.now() - 60 * 1000)
 // // "1 minute ago"
-
-// timeAgo.format(Date.now() - 2 * 60 * 60 * 1000)
-// // "2 hours ago"
-
-// timeAgo.format(Date.now() - 24 * 60 * 60 * 1000)
-// // "1 day ago"
 
 module.exports = timeAgo
