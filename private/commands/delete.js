a6f853fff936f716ce2b679445d4f0a68cb39af0
bartick <bartickmaiti1998@gmail.com>
2021-09-22 15:31:47 +0530

    Delete orders by force

---
+++ private/commands/delete.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const orders = require('./../../models/orders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('delete')
+        .setDescription('Delete any order.')
+        .addIntegerOption( option =>
+            option.setName('orderid')
+                .setDescription('Enter the order id')
+                .setRequired(true)
+        )
+        .addStringOption( option => 
+            option.setName('customerid')
+                .setDescription('Enter the customerid')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        await interaction.deferReply({
+            ephemeral: true
+        });
+        const orderId = interaction.options.getInteger('orderid');
+        const customerId = interaction.options.getString('customerid');
+        const order = await orders.deleteOne({
+            orderid: orderId,
+            customerid: customerId
+        });
+        if (order.deletedCount==0) {
+            await interaction.editReply({
+                content: `There is no order with id ${orderId}`
+            });
+        }
+        else {
+            await interaction.editReply({
+                content: `Successfully deleted order with id ${orderId}`
+            });
+        }
+    }
+};
