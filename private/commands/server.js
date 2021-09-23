229e0357ea8fb2f1ab32718901cc6f41200943a8
bartick <bartickmaiti1998@gmail.com>
2021-09-23 10:01:08 +0530

    Added a new command

---
+++ private/commands/server.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Check the info about the bot in different servers.'),
+    async execute(interaction) {
+        await interaction.deferReply()
+        const size = interaction.client.guilds.cache.size;
+        const ram = process.memoryUsage().heapUsed / 1024 / 1024;
+        await interaction.editReply(`I am in ${size} servers.\nI am approximately using ${Math.round(ram * 100) / 100} MB`)
+    }
+};
