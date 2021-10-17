247fe4aeb06d50e8466d0ebd5422e2a9bfb8fd2f
bartick <bartickmaiti1998@gmail.com>
2021-10-17 18:38:07 +0530

    Fixed typo

--- commands/discount.js
+++ commands/discount.js
@@ -207,7 +207,7 @@ module.exports = {
                         },
                         {
                             $set: {
-                                disRole: orderDis
+                                disOrder: orderDis
                             }
                         }
                     )
