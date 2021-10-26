7ef97b1f716ec9eb26627753e53c58b7d3559f9c
bartick <bartickmaiti1998@gmail.com>
2021-10-26 13:46:15 +0530

    New implementation style

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,21 +4,22 @@ module.exports = {
 	async execute(interaction) {
 		if (interaction.isCommand()) {
 			const command = interaction.client.commands.get(interaction.commandName);
-	
-			try {
-				await command.execute(interaction);
-			} catch (error) {
-				console.error(error);
-				try {
-					await interaction.reply({ content: 'There was an error while executing this command! ', ephemeral: true });
-				} catch (err) {
-					try {
-						await interaction.editReply({ content: 'There was an error while executing this command! ', ephemeral: true });
-					} catch (e) {
-						// SKIP
-					}
-				}
-			}
+
+			await command.execute(interaction)
+					.catch(error => {
+						console.error(error);
+						if (interaction.replied) {
+							await interaction.editReply({ content: 'There was an error while executing this command! Please report this to **Bartick**', ephemeral: true })
+									.catch(err => {
+										//SKIP
+									})
+						} else {
+							await interaction.reply({ content: 'There was an error while executing this command! Please report this to **Bartick**', ephemeral: true })
+									.catch(err => {
+										//SKIP
+									})
+						}
+					});
 		}
 	},
 };
