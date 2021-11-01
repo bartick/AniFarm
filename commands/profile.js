b48b97ab9c3b7721cd12db308c8ef5661a798bb5
bartick <bartickmaiti1998@gmail.com>
2021-11-01 11:46:56 +0530

    Now people will be able to register for profiles

--- commands/profile.js
+++ commands/profile.js
@@ -8,6 +8,10 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('profile')
         .setDescription('Play with profile')
+        .addSubcommand(subcommand => 
+            subcommand.setName('register')
+                .setDescription('Register for a profile')
+        )
         .addSubcommand(subcommand =>
             subcommand.setName('view')
                 .setDescription('View a profile')
@@ -30,7 +34,38 @@ module.exports = {
         ),
     async execute(interaction) {
         const subcommand = interaction.options.getSubcommand();
-        if (subcommand==='view') {
+        if (subcommand==='register'){
+            await interaction.deferReply()
+            const newProfile = new anifarm({
+                _id: interaction.user.id
+            });
+            await newProfile.save()
+                    .catch(async err => {
+                        await interaction.editReply({
+                            embeds: [
+                                new MessageEmbed()
+                                    .setColor('FF0000')
+                                    .setTitle('⛔ Error')
+                                    .setTimestamp()
+                                    .setThumbnail(interaction.client.user.displayAvatarURL({dynamic: true, size: 1024}))
+                                    .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({dynamic: true, size: 1024}))
+                                    .setDescription(`**${interaction.user.tag}** you have already registered. You don't need to use this command any more. Enjoy all the other commands.`)
+                            ]
+                        })
+                    }).then(async inter => {
+                        await interaction.editReply({
+                            embeds: [
+                                new MessageEmbed()
+                                    .setColor('00FF00')
+                                    .setTitle('🎉 Registered')
+                                    .setTimestamp()
+                                    .setThumbnail(interaction.client.user.displayAvatarURL({dynamic: true, size: 1024}))
+                                    .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({dynamic: true, size: 1024}))
+                                    .setDescription(`**${interaction.user.tag}** you have successfully registered. You don't need to use this command any more. Enjoy all the other commands.`)
+                            ]
+                        })
+                    })
+        } else if (subcommand==='view') {
             const userToFind = interaction.options.getUser('user') || interaction.user;
             const player = await anifarm.findById(userToFind.id);
             if (player===null) {
