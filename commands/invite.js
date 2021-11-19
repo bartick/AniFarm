e20cb43e43f6c91a8a0842b2e71a095d030e96a0
bartick <bartickmaiti1998@gmail.com>
2021-11-19 12:22:01 +0530

    changed link to support server

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
             .setDescription('Invite me'),
     async execute(interaction) {
         const invite = 'https://discord.com/api/oauth2/authorize?client_id=816544600118132747&permissions=137707899968&scope=bot%20applications.commands';
-        const support = 'https://discord.gg/wR8uJkT9NT';
+        const support = 'https://discord.gg/7VTFyASFTb';
         await interaction.reply({
             content: `To invite me to your server use the link below \n${invite} \nIf you have any problem setting up the bot you can join my support server for help.\n${support}`
         });
