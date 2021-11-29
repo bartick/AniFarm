af4328ff9ddb002e84d417557d98786cc1295162
bartick <bartickmaiti1998@gmail.com>
2021-11-29 12:02:33 +0530

    Removed unused function

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,20 +1,6 @@
 'use strict';
 const { MessageEmbed } = require('discord.js');
 
-function getUserFromMention(client, mention) {
-	if (!mention) return;
-
-	if (mention.startsWith('<@') && mention.endsWith('>')) {
-		mention = mention.slice(2, -1);
-
-		if (mention.startsWith('!')) {
-			mention = mention.slice(1);
-		}
-
-		return client.users.cache.get(mention);
-	}
-}
-
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
