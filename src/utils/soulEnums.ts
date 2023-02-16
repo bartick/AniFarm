12b7929309e922f820868fa3586750ada16711c2
bartick <bartickmaiti1998@gmail.com>
2023-02-16 17:48:07 +0530

    added enums for soul shards

---
+++ src/utils/soulEnums.ts
@@ -0,0 +1,43 @@
+
+export const WaterSoulShards = "<:WaterSoulShards:1075633632024215612>";
+
+export const FireSoulShards = "<:FireSoulShards:1075633954889142292>";
+
+export const GroundSoulShards = "<:GroundSoulShards:1075634616733536316>";
+
+export const LightSoulShards = "<:LightSoulShards:1075634641777737848>";
+
+export const DarkSoulShards = "<:DarkSoulShards:1075634686015049759>";
+
+export const NeutralSoulShards = "<:NeutralSoulShards:1075634720764866570>";
+
+export const GrassSoulShards = "<:GrassSoulShards:1075634752180195408>";
+
+export const ElectricSoulShards = "<:ElectricSoulShards:1075634768156315659>";
+
+export const Water = "water";
+
+export const Fire = "fire";
+
+export const Ground = "ground";
+
+export const Light = "light";
+
+export const Dark = "dark";
+
+export const Neutral = "neutral";
+
+export const Grass = "grass";
+
+export const Electric = "electric";
+
+export const SoulEnums = {
+    Water: WaterSoulShards,
+    Fire: FireSoulShards,
+    Ground: GroundSoulShards,
+    Light: LightSoulShards,
+    Dark: DarkSoulShards,
+    Neutral: NeutralSoulShards,
+    Grass: GrassSoulShards,
+    Electric: ElectricSoulShards,
+}
