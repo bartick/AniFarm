e94ac8471ead4287e72634fc6ac9d633adc3608e
bartick <bartickmaiti1998@gmail.com>
2023-02-22 19:06:06 +0530

    change: cards -> soul

--- src/commands/farming.ts
+++ src/commands/farming.ts
@@ -1,97 +1,45 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { MessageEmbed } from "discord.js";
-import { Command, CustomCommandInteraction } from "../interfaces";
-import { OrdersType } from "../schema";
+import { 
+    SlashCommandBuilder 
+} from "@discordjs/builders";
+import { 
+    Command, 
+    CustomCommandInteraction 
+} from "../interfaces";
+import { 
+    OrderManager 
+} from "../utils";
 
-import { mongodb } from "../utils";
-
-const Orders = mongodb.models['orders'];
- 
 const farming: Command = {
     data: new SlashCommandBuilder()
         .setName("farming")
         .setDescription("Shows what you are farming currently"),
     execute: async (interaction: CustomCommandInteraction) => {
         await interaction.deferReply({
-            ephemeral: true,
-        });
-        const farmingList: OrdersType | null = await Orders.findOne({
-            farmerid: interaction.user.id,
+            ephemeral: true
         });
 
-        // if the user is not farming anything, tell them so.
-        if (farmingList === null) {
-            await interaction.editReply({
+        const Manager = new OrderManager(interaction);
+        const check = await Manager.getOrderByFarmer();
+
+        if(!check) {
+            interaction.editReply({
                 embeds: [
-                    new MessageEmbed()
-                        .setColor('#ff0000')
-                        .setTitle('⛔️ Error')
-                        .setDescription('You are not farming anything.')
-                        .setTimestamp()
-                        .setThumbnail(interaction.client.user?.displayAvatarURL({
-                            dynamic: true,
-                            size: 1024,
-                        }) || '')
-                        .setAuthor({
-                            name: interaction.user.username,
-                            iconURL: interaction.user.displayAvatarURL({
-                                dynamic: true,
-                                size: 1024,
-                            }),
-                        }),
-                ],
+                    Manager.errorEmbed("You are not farming anything")
+                ]
             });
+
             return;
         }
 
-        const customer = await interaction.client.users.fetch(farmingList.customerid).catch(() => {});
-        const guild = interaction.client.guilds.cache.get(farmingList.guildid);
-        const embed = new MessageEmbed()
-                .setColor('#00ff00')
-                .setTitle('Farming Status')
-                .setFields([
-                    {
-                        name: 'Farmer:',
-                        value: interaction.user.username,
-                        inline: true,
-                    },
-                    {
-                        name: 'Customer:',
-                        value: customer?.username || 'Unknown',
-                        inline: true, 
-                    },
-                    {
-                        name: 'Guild:',
-                        value: guild?.name || 'Unknown',
-                        inline: false
-                    },
-                    {
-                        name: 'Order Summary:',
-                        value: `${"```"}\n◙ Card Name: ${farmingList.name}\n◙ Loc-Floor: ${farmingList.location}-${farmingList.floor}\n◙ Amount: ${farmingList.amount}\n◙ Price: ${farmingList.price - Math.trunc(farmingList.price*farmingList.discount/100)}\n◙ Discount: ${farmingList.discount} \n${"```"}`,
-                        inline: false,
-                    }
-                ])
-                .setThumbnail(farmingList.image)
-                .setTimestamp()
-                .setAuthor({
-                    name: customer?.username || 'Unknown',
-                    iconURL: customer?.displayAvatarURL({
-                        dynamic: true,
-                        size: 1024,
-                    }) || '',
-                })
-                .setFooter({
-                    text: `${interaction.user.username} • Order Id ${farmingList.orderid}`,
-                    iconURL: interaction.user.displayAvatarURL({
-                        dynamic: true,
-                        size: 1024,
-                    })
-                })
-        await interaction.editReply({
-            embeds: [embed],
-        })
-    }
-}
+        const embed = await Manager.completedOrderEmbed();
 
+        interaction.editReply({
+            embeds: [
+                embed
+            ]
+        });
+
+    }
+};
 
 export default farming;
--- src/commands/orders.ts
+++ src/commands/orders.ts
@@ -1,101 +1,42 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
-import { MessageEmbed, Message } from "discord.js";
-import { Command, CustomCommandInteraction } from "../interfaces";
-import { OrdersType } from "../schema";
-
-import { mongodb, paginate } from "../utils";
-
-const Orders = mongodb.models['orders'];
+import { 
+    SlashCommandBuilder 
+} from "@discordjs/builders";
+import { 
+    Message 
+} from "discord.js";
+import { 
+    Command, 
+    CustomCommandInteraction 
+} from "../interfaces";
+import { 
+    OrderManager,
+    paginate
+} from "../utils";
 
 const orders: Command = {
     data: new SlashCommandBuilder()
         .setName("orders")
         .setDescription("Shows all of your orders"),
     execute: async (interaction: CustomCommandInteraction) => {
-        const messageReply = await interaction.deferReply({
+        const message = await interaction.deferReply({
             fetchReply: true
-        }) as Message<boolean>
+        }) as Message<boolean>;
 
-        const ordersList: Array<OrdersType> = await Orders.find({
-            customerid: interaction.user.id,
-        });
+        const Manager = new OrderManager(interaction);
+        const embeds = await Manager.checkOrderPlacedEmbeds();
 
-        // if the user is not farming anything, tell them so.
-        if (ordersList.length === 0) {
-            await interaction.editReply({
+        if(embeds.length === 0) {
+            interaction.editReply({
                 embeds: [
-                    new MessageEmbed()
-                        .setColor('#ff0000')
-                        .setTitle('⛔️ Error')
-                        .setDescription('You have no orders queued.')
-                        .setTimestamp()
-                        .setThumbnail(interaction.client.user?.displayAvatarURL({
-                            dynamic: true,
-                            size: 1024,
-                        }) || '')
-                        .setAuthor({
-                            name: interaction.user.username,
-                            iconURL: interaction.user.displayAvatarURL({
-                                dynamic: true,
-                                size: 1024,
-                            }),
-                        }),
-                ],
+                    Manager.errorEmbed("You have no orders")
+                ]
             });
+
             return;
         }
 
-        const embeds: Array<MessageEmbed> = [];
-        for (const order of ordersList) {
-            const guild = interaction.client.guilds.cache.get(order.guildid);
-            const farmer = await interaction.client.users.fetch(order.farmerid).catch(() => null);
-            embeds.push(
-                new MessageEmbed()
-                    .setColor('#00ffff')
-                    .setTitle('Order Status')
-                    .setFields([
-                        {
-                            name: 'Farmer:',
-                            value: (farmer?.username || 'Unknown') + '#' + (farmer?.discriminator || '0000'),
-                            inline: true,
-                        },
-                        {
-                            name: 'Customer:',
-                            value: interaction.user.username + '#' + interaction.user.discriminator,
-                            inline: true, 
-                        },
-                        {
-                            name: 'Guild:',
-                            value: guild?.name || 'Unknown',
-                            inline: false
-                        },
-                        {
-                            name: 'Order Summary:',
-                            value: `${"```"}\n◙ Card Name: ${order.name}\n◙ Loc-Floor: ${order.location}-${order.floor}\n◙ Amount: ${order.amount}\n◙ Price: ${order.price - Math.trunc(order.price*order.discount/100)}\n◙ Discount: ${order.discount} \n${"```"}`,
-                            inline: false,
-                        }
-                    ])
-                    .setThumbnail(order.image)
-                    .setTimestamp()
-                    .setAuthor({
-                        name: interaction.user.username,
-                        iconURL: interaction.user.displayAvatarURL({
-                            dynamic: true,
-                            size: 1024,
-                        }),
-                    })
-                    .setFooter({
-                        text: `${farmer?.username || 'Unknown'} • Order Id ${order.orderid}`,
-                        iconURL: farmer?.displayAvatarURL({
-                            dynamic: true,
-                            size: 1024,
-                        })|| ''
-                    })
-            );
-        }
+        await paginate(interaction, embeds, message)
 
-
-        await paginate(interaction, embeds, messageReply);
     }
 }
 
